054331a451af7a0101885472a4b0cbfb0bc46251
Huynh Tran Ty <22120418@student.hcmus.edu.vn>
2025-07-15 08:36:04 +0700

    refactor wiki code

--- backend/src/main/java/com/hcmus/softdes/aivideocreator/infrastructure/external/wiki/WikipediaService.java
+++ backend/src/main/java/com/hcmus/softdes/aivideocreator/infrastructure/external/wiki/WikipediaService.java
@@ -1,9 +1,5 @@
 package com.hcmus.softdes.aivideocreator.infrastructure.external.wiki;
 
-import com.hcmus.softdes.aivideocreator.domain.model.TopicWiki;
-
-import java.util.List;
-
 public interface WikipediaService {
     public String getShortSummary(String keyword, int lines);
 }
